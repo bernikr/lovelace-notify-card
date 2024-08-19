30a83acc1a9cbea46e89229a9cb67eeea57d0239
Bernie <git@berni.kr>
2024-08-19 18:57:07 +0200

    allow any service to be used

--- notify-card.js
+++ notify-card.js
@@ -1,15 +1,17 @@
 class NotifyCard extends HTMLElement {
   setConfig(config) {
-    if (!config.target) {
-      throw new Error('You need to define one or more targets');
+    if (!config.target && (!config.action || !config.data)) {
+      throw new Error('You need to define an action and data');
     }
     this.config = config;
-    if (typeof this.config.target == "string") {
-      this.targets = [this.config.target];
-    } else if (Array.isArray(this.config.target)) {
-      this.targets = this.config.target
-    } else {
-      throw new Error('Target needs to be a list or single target');
+    if(config.target){
+      if (typeof this.config.target == "string") {
+        this.targets = [this.config.target];
+      } else if (Array.isArray(this.config.target)) {
+        this.targets = this.config.target
+      } else {
+        throw new Error('Target needs to be a list or single target');
+      }
     }
     this.render();
   }
@@ -48,21 +50,46 @@ class NotifyCard extends HTMLElement {
   }
 
   send(){
+    const dict_replace = (dict, terms) => {
+      if(Array.isArray(dict)) {
+        return dict.map(t => dict_replace(t, terms));
+      }
+      
+      if(typeof dict === 'object' && dict !== null) {
+        return Object.fromEntries(
+          Object.entries(dict)
+            .map(([k, v]) => [k, dict_replace(v, terms)]
+          )
+        );
+      }
+      
+      return dict in terms ? terms[dict] : dict;
+    };
+
+
     let msg = this.content.querySelector("#notification_text").value;
     let title = this.content.querySelector("#notification_title")?.value ?? this.config.notification_title;
-    for (let t of this.targets) {
-      let [domain, target = null] = t.split(".");
-      if(target === null){
-        target = domain;
-        domain = "notify";
-      }
 
-      if(domain === "tts"){
-        this.hass.callService(domain, target, {"entity_id": this.config.service, "media_player_entity_id": this.config.entity, "message": msg});
-      } else {
-        this.hass.callService(domain, target, {message: msg, title: title, data: this.config.data});
+    if(this.config.action){
+      let data = dict_replace(this.config.data, {"$MSG": msg, "$TITLE": title})
+      let [domain, service] = this.config.action.split(".");
+      this.hass.callService(domain, service, data);
+    }else{
+      for (let t of this.targets) {
+        let [domain, target = null] = t.split(".");
+        if(target === null){
+          target = domain;
+          domain = "notify";
+        }
+  
+        if(domain === "tts"){
+          this.hass.callService(domain, target, {"entity_id": this.config.service, "media_player_entity_id": this.config.entity, "message": msg});
+        } else {
+          this.hass.callService(domain, target, {message: msg, title: title, data: this.config.data});
+        }
       }
     }
+
     this.content.querySelectorAll("ha-textfield").forEach(e => e.value = "");
   }
 
